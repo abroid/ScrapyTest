5997e6e89f1c1fd59f5a5c5b7b197136783bc1df
Abdul Roid <82431988+abroid@users.noreply.github.com>
2023-08-15 13:02:26 +0700

    Upload Files

---
+++ quotes.py
@@ -0,0 +1,29 @@
+import scrapy
+
+class Quote(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'https://quotes.toscrape.com/'
+    ]
+
+    def parse(self, response):
+        all_quotes = response.css("div.quote")
+        for quotes in all_quotes:
+            content = quotes.css(".text::text").get()
+            author = quotes.css(".author::text").get()
+            tag = quotes.css(".tag::text").getall()
+
+            yield {
+                'content':content,
+                'author':author,
+                'tags':tag
+            }
+    
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+        else:
+            pass
+
+
+
